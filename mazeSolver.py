c3a662d9126e2dc330747cd705a5261280371eb3
unethical <joasepul@ucsc.edu>
2017-08-25 20:12:27 -0700

    first push

---
+++ mazeSolver.py
@@ -0,0 +1,14 @@
+from PIL import Image
+
+im = Image.open("maze.jpg")
+pixelMatrix = im.load()
+
+start = (185, 0)
+end = (403, 375)
+
+x, y = im.size
+
+print("TODO: actually solve the f****** maze")
+    
+
+
